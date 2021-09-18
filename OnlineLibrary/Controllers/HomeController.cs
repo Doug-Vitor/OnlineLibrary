b9310a24d79a7568b1d9976720bb47f95afcc3a6
Douglas Vitor <douglasvitor.dev@gmail.com>
2021-09-18 00:06:37 -0300

    Initial commit

---
+++ OnlineLibrary/Controllers/HomeController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace OnlineLibrary.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
